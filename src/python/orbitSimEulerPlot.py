752db9af0063618bb6b9086df8992433fa1e6cc6
Jacob Dent <murkymicrobe88@gmail.com>
2025-03-25 16:43:35 -0400

    Displays the points on a 3D projection

--- src/python/orbitSimEulerPlot.py
+++ src/python/orbitSimEulerPlot.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.pyplot as plt
 
 
 
@@ -14,19 +15,28 @@ def main():
     coreDist = 6377992.206
     # Distance from surface to orbit
     surfaceDist = 2000000
+
+    # Lists to store position data for plotting
+    x_vals, y_vals, z_vals = [], [], []
     
     
     # Sattelite Starting position vecotr
     Pos = [8377992.206, 0, 0]
     # Sattelite Starting velocity vector
-    Vel = [0, 7120, 0]
+    Vel = [0, 6800 , 0]
 
     # Unit vector temp variable
     unitV = [0,0,0]
     
     # Calculate the orbit of the sattelite
 
-    for i in range(1000):
+    for i in range(50000):
+
+        # Store current position for visualization
+        x_vals.append(Pos[0])
+        y_vals.append(Pos[1])
+        z_vals.append(Pos[2])
+
 
         # Magnitute of position
         MagPos = math.sqrt(Pos[0]**2 + Pos[1]**2 + Pos[2]**2)
@@ -46,8 +56,23 @@ def main():
         for j in range(3):
             Vel[j] = Vel[j] + (a * unitV[j])
 
-        #print(f"t: {i} Position: {Pos} Velocity: {Vel}")
-        print(int(Pos))
+    # Plot the orbit
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plot satellite trajectory
+    ax.plot(x_vals, y_vals, z_vals, label="Satellite Orbit", color="blue")
+
+    # Plot Earth (as a point)
+    ax.scatter(0, 0, 0, color="red", s=100, label="Earth")
+
+    # Labels and legend
+    ax.set_xlabel("X Position (m)")
+    ax.set_ylabel("Y Position (m)")
+    ax.set_zlabel("Z Position (m)")
+    ax.legend()
+    plt.show()
+
 
 
 
